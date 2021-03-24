a4ba92d58c1256a2e3340776782470e3467bdf51
Bartosz Ciesielczyk <purpleturtll@gmail.com>
2021-03-24 20:37:04 +0100

    usunięcie username z user, logowanie emailem

--- resource_server/models/User.go
+++ resource_server/models/User.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username  string `json:"username"`
 	Password  string `json:"password"`
 	Firstname string `json:"firstname"`
 	Surname   string `json:"surname"`
