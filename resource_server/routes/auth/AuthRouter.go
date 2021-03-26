533c91b67d701a10534020b9e2614dc9810c9588
purpleturtll <purpleturtll@gmail.com>
2021-03-26 16:18:17 +0100

    ustawienie zgodnych nazw funkcji i struktur

--- resource_server/routes/auth/AuthRouter.go
+++ resource_server/routes/auth/AuthRouter.go
@@ -2,9 +2,9 @@ package auth
 
 import "github.com/labstack/echo/v4"
 
-type AuthRouter struct{}
+type Router struct{}
 
-func (AuthRouter) Init(g *echo.Group) {
+func (Router) Init(g *echo.Group) {
 	g.POST("/login", Login)
 	g.POST("/register", Register)
 }
--- resource_server/routes/routes.go
+++ resource_server/routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"inzynierka/routes/animal"
+	"inzynierka/routes/auth"
 
 	"github.com/labstack/echo/v4"
 )
 
 func Init(g *echo.Group) {
 	animal.Router{}.Init(g.Group("/animal"))
+	auth.Router{}.Init(g.Group("/auth"))
 }
