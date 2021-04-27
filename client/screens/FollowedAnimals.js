23999691c5bca6a802c2d360924c6f1e47218423
Mimak <mim4k1@gmail.com>
2021-04-27 18:16:04 +0200

    lista ulubionych

--- client/screens/FollowedAnimals.js
+++ client/screens/FollowedAnimals.js
@@ -1,16 +1,80 @@
-import React from 'react';
-import {
-  View, Text, StyleSheet, TouchableOpacity
-} from 'react-native';
+import React, {useState} from 'react';
+import { View,  StyleSheet, FlatList } from 'react-native';
+import AnimalCard from '../components/AnimalCard'
 
 const TempScreen = ({ navigation }) => {
+  
+  const [animals, setAnimals] = useState([
+    {
+      id: '1',
+      name: 'Angus',
+      type: 'pies',
+      race: 'mieszaniec',
+      sex: 'samiec',
+      postDate: '6/3/21',
+      favourite: true,
+      status: [{adoptable: true}, {urgent: true}],
+      weight: 9,
+      ageMonths: 156,
+      city: 'Warszawa',
+      location: 'Schronisko dla Bezdomnych Zwierząt w Warszawie',
+      takeInDate: '20-11-2020',
+      description: 'Opis Angusa',
+      imageUrl: '',
+      image: 'dog1'
+    },
+    {
+      id: '2',
+      name: 'Mruczek',
+      type: 'kot',
+      race: 'europejska',
+      sex: 'samiec',
+      postDate: '2/3/21',
+      favourite: true,
+      status: [{adoptable: true}],
+      weight: 7,
+      ageMonths: 60,
+      city: 'Otwock',
+      location: 'Schronisko dla Bezdomnych Zwierząt w Otwocku',
+      takeInDate: '24-12-2020',
+      description: 'Opis Mruczka',
+      imageUrl: '',
+      image: 'cat1'
+    }
+  ]);
+
+  const handleFavChange = (id) => {
+    animals.map(animal => {
+      if(animal.id==id){
+        setAnimals({
+          ...animals,
+          favourite: !animal.favourite,
+      });
+      }
+      console.log(animal.favourite);
+    })
+  }
+  
   return(
     <View style={styles.container}>
     <View style={styles.header}>
+
+    {/*Lista zwierzaków*/}  
+    <View style={{alignItems: 'center'}}>
+      <FlatList 
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id }
+        data={animals}
+        renderItem={({ item }) => (
+          <AnimalCard 
+            animal={item} 
+            onFavChange={() => handleFavChange}
+          />
+        )}
+      />
     </View>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignInScreen')}>
-        <Text>Followed</Text>
-      </TouchableOpacity>
+      
+    </View>
     </View>
   )
 }
