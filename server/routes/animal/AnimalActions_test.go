d746c26bba617ebb099cb3cb3af5612c84a6ae2a
unknown <bartoszcz616@gmail.com>
2021-06-15 21:37:09 +0200

    Wiecej testow filtra po stronie serwera

--- server/routes/animal/AnimalActions_test.go
+++ server/routes/animal/AnimalActions_test.go
@@ -15,7 +15,7 @@ func TestFilter(t *testing.T) {
 	db.Migrate()
 
 	e := echo.New()
-	req := httptest.NewRequest("GET", "http://127.0.0.1:8080/animal/read?page=0&animal-type=pies", nil)
+	req := httptest.NewRequest("GET", "http://127.0.0.1:8080/animal/read?page=0&animal-type=pies,kot", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	ctx := e.NewContext(req, rec)
@@ -31,14 +31,46 @@ func TestFilterSpeed(t *testing.T) {
 	db.Migrate()
 
 	e := echo.New()
-	req := httptest.NewRequest("GET", "http://127.0.0.1:8080/animal/read?page=0&animal-type=pies", nil)
+	req := httptest.NewRequest("GET", "http://127.0.0.1:8080/animal/read?page=0&animal-type=pies,kot", nil)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	ctx := e.NewContext(req, rec)
 	if assert.NoError(t, Filter(ctx)) {
-		for i := 0; i < 1000000; i++ {
+		for i := 0; i < 5000000; i++ {
 			assert.Equal(t, http.StatusOK, rec.Code)
 		}
+	}
+
+	db.CleanTest()
+}
+
+func TestFilterWrongFilter(t *testing.T) {
+	db.InitTest()
+	db.Migrate()
+
+	e := echo.New()
+	req := httptest.NewRequest("GET", "http://127.0.0.1:8080/animal/read?page=0&animal=pies,kot", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	if assert.NoError(t, Filter(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+
+	db.CleanTest()
+}
+
+func TestFilterWrongData(t *testing.T) {
+	db.InitTest()
+	db.Migrate()
+
+	e := echo.New()
+	req := httptest.NewRequest("GET", "http://127.0.0.1:8080/animal/read?page=0&age-from=abc", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	if assert.NoError(t, Filter(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
 	}
 
 	db.CleanTest()
