d73426c1c98e9b1af5d05f199706e71d7cc819cc
unknown <bartoszcz616@gmail.com>
2021-09-21 18:41:44 +0200

    Zmiana info o zwierzeciu, ktore jest wysylane do klienta

--- server/routes/animal/AnimalActions.go
+++ server/routes/animal/AnimalActions.go
@@ -8,12 +8,63 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
 
+// Wielkość strony do wysłania
 const pageSize = 2
+
+// Struktura z danymi do wysłania
+type AnimalSend struct {
+	AnimalType    string    `json:"type"`
+	Breed         string    `json:"breed"`
+	Name          string    `json:"name"`
+	ShelterCity   string    `json:"city"`
+	Adoptable     bool      `json:"adoptable"`
+	Description   string    `json:"description"`
+	Age           uint      `json:"age"`
+	Weight        float32   `json:"weight"`
+	Sex           string    `json:"sex"`
+	AdmissionDate time.Time `json:"admission_date"`
+	ChipNumber    string    `json:"chip_number"`
+	RecentlyFound bool      `json:"recently_found"`
+	IsSterilized  bool      `json:"is_sterilized"`
+	IsVaccinated  bool      `json:"is_vaccinated"`
+}
+
+// Przenoszenie danych ze structa Animal do AnimalSend
+func AnimalConvert(animals_db []models.Animal) []AnimalSend {
+	var animals []AnimalSend
+	var animalType models.AnimalType
+	var shelter models.Shelter
+
+	for _, v := range animals_db {
+		db.Connection().Select("type").Where("id = ?", v.AnimalTypeID).First(&animalType)
+		db.Connection().Select("city").Where("id = ?", v.ShelterID).First(&shelter)
+		animal := AnimalSend{
+			AnimalType:    animalType.Type,
+			Breed:         v.Breed,
+			Name:          v.Name,
+			ShelterCity:   shelter.City,
+			Adoptable:     v.Adoptable,
+			Description:   v.Description,
+			Age:           v.Age,
+			Weight:        v.Weight,
+			Sex:           v.Sex,
+			AdmissionDate: v.AdmissionDate,
+			ChipNumber:    v.ChipNumber,
+			RecentlyFound: v.RecentlyFound,
+			IsSterilized:  v.IsSterilized,
+			IsVaccinated:  v.IsVaccinated,
+		}
+		animals = append(animals, animal)
+	}
+
+	return animals
+}
 
 func Create(c echo.Context) error {
 	obj := new(models.Animal)
@@ -50,7 +101,9 @@ func ReadChip(c echo.Context) error {
 
 // Filtrowanie
 func Filter(c echo.Context) error {
-	var animals []models.Animal
+	var animals_db []models.Animal
+	var animals []AnimalSend
+
 	animalType := c.QueryParam("animal-type")
 	sex := c.QueryParam("sex")
 	city := c.QueryParam("city")
@@ -66,10 +119,15 @@ func Filter(c echo.Context) error {
 	result := db.Connection().Limit(pageSize).Offset(pageSize*pageInt).Scopes(
 		Sex(sex), City(strings.Split(city, ",")), AgeRange([][]string{strings.Split(ageFrom, ","), strings.Split(ageTo, ",")}), Favourite(favourite),
 		WeightRange([][]string{strings.Split(weightFrom, ","), strings.Split(weightTo, ",")}), Breed(strings.Split(breed, ",")), AnimalType(strings.Split(animalType, ",")),
-	).Find(&animals)
+	).Find(&animals_db)
 	if result.Error == gorm.ErrRecordNotFound {
 		return c.String(http.StatusNotFound, "Not Found")
 	}
+
+	// Przygotowanie danych do wysłania
+	animals = AnimalConvert(animals_db)
+	fmt.Print(animals)
+
 	return c.JSON(http.StatusOK, animals)
 }
 
