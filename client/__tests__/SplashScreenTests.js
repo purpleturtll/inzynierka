dd2341e3c71dc9c6dbe1c80a78b7c6cbcfbae4b9
Anna Ciszak <aniac600@gmail.com>
2021-11-03 15:09:27 +0100

    Dodanie SplashScreenTests

---
+++ client/__tests__/SplashScreenTests.js
@@ -0,0 +1,10 @@
+// import { create, act } from "react-test-renderer";
+// import SplashScreen from "../screens/SplashScreen";
+
+// const tree = create(<SplashScreen/>);
+
+// jest.runAllTimers();
+
+// test('snapshot', () => {
+//   expect(tree).toMatchSnapshot();
+// }); 
