10568d9915e0166a3422aa436e3bc6be3221e9ff
Anna Ciszak <aniac600@gmail.com>
2022-01-03 23:27:41 +0100

    Zmiana koloru serca Ulubione

--- client/components/AnimalCard.js
+++ client/components/AnimalCard.js
@@ -1,44 +1,53 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { AntDesign } from '@expo/vector-icons';
-import { AnimalDataContext } from '../contexts/AnimalContext';
-import Constants from 'expo-constants';
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { AntDesign } from "@expo/vector-icons";
+import { AnimalDataContext } from "../contexts/AnimalContext";
+import Constants from "expo-constants";
 const apiUrl = Constants.manifest.extra.apiUrl;
 export default AnimalCard = ({ animalId, navigation }) => {
-
-  {/*Kontekst zwierząt*/ }
+  {
+    /*Kontekst zwierząt*/
+  }
   const animalCtx = useContext(AnimalDataContext);
   var animal = animalCtx.getAnimal(animalId);
 
-  {/*Funkcja nawigująca do AnimalDetailsScreen (klik na obrazku AnimalCard)*/ }
+  {
+    /*Funkcja nawigująca do AnimalDetailsScreen (klik na obrazku AnimalCard)*/
+  }
   function onAnimalPress(anim) {
-    navigation.navigate('AnimalDetailsScreen', anim);
+    navigation.navigate("AnimalDetailsScreen", anim);
   }
 
-  {/*Pomocnicza*/ }
+  {
+    /*Pomocnicza*/
+  }
   function age2Polish(age) {
     switch (age) {
       case 1:
-        return 'rok';
+        return "rok";
       case 2:
       case 3:
       case 4:
-        return 'lata';
+        return "lata";
       default:
-        return 'lat';
+        return "lat";
     }
   }
 
-  {/*Nazwy ikon serduszek*/ }
+  {
+    /*Nazwy ikon serduszek*/
+  }
   const hearts = {
     icon: {
-      true: 'heart',
-      false: 'hearto',
-    }
-  }
+      true: "heart",
+      false: "hearto",
+    },
+  };
 
-  {/*Karta*/ }
+  {
+    /*Karta*/
+  }
   return (
     <View style={styles.card}>
       <TouchableOpacity onPress={() => onAnimalPress(animal)}>
@@ -48,7 +57,7 @@ export default AnimalCard = ({ animalId, navigation }) => {
         />
       </TouchableOpacity>
       {/*info*/}
-      <View style={{ padding: 10, flexDirection: 'row' }}>
+      <View style={{ padding: 10, flexDirection: "row" }}>
         {/*blok tekstu*/}
         <View>
           {/*tytuł*/}
@@ -58,55 +67,66 @@ export default AnimalCard = ({ animalId, navigation }) => {
           </View>
           {/*TODO: wyświetlanie wieku w miesiącach*/}
           <Text>{animal.breed}</Text>
-          <Text>{animal.sex}, {animal.age} {age2Polish(animal.age)}, {animal.weight} kg</Text>
+          <Text>
+            {animal.sex}, {animal.age} {age2Polish(animal.age)}, {animal.weight}{" "}
+            kg
+          </Text>
         </View>
         {/*kolor serduszka #ff4242*/}
-        <TouchableOpacity onPress={() => {
-          {/*stan globalny w AnimalDataContext, TODO: animalCtx.getAnimal(id)*/ }
-          animalCtx.updateFavourite(animal.id);
-        }}>
+        <TouchableOpacity
+          onPress={() => {
+            {
+              /*stan globalny w AnimalDataContext, TODO: animalCtx.getAnimal(id)*/
+            }
+            animalCtx.updateFavourite(animal.id);
+          }}
+        >
           <View style={styles.heart}>
-            <AntDesign name={hearts.icon[animal.favourite]} size={30} color='black' />
+            <AntDesign
+              name={hearts.icon[animal.favourite]}
+              size={30}
+              color="#d12115"
+            />
           </View>
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   card: {
     marginVertical: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 15,
     elevation: 6,
-    shadowColor: '#fff',
+    shadowColor: "#fff",
     shadowOffset: { width: 10, height: 10 },
     shadowOpacity: 0.5,
     shadowRadius: 5,
-    overflow: 'hidden'
+    overflow: "hidden",
   },
   headline: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingTop: 3,
     paddingBottom: 5,
   },
   headlineName: {
-    fontWeight: 'bold',
-    fontSize: 16
+    fontWeight: "bold",
+    fontSize: 16,
   },
   headlineDate: {
     fontSize: 16,
   },
   image: {
-    resizeMode: 'cover',
+    resizeMode: "cover",
     width: 300,
     height: 200,
   },
   heart: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: '35%',
-    marginTop: 18
-  }
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: "35%",
+    marginTop: 18,
+  },
 });
--- client/screens/AnimalDetailsScreen.js
+++ client/screens/AnimalDetailsScreen.js
@@ -1,116 +1,116 @@
-import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { AntDesign } from '@expo/vector-icons';
-import DataRow from '../components/DataRow';
-import StatusDataRow from '../components/StatusDataRow'
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { AntDesign } from "@expo/vector-icons";
+import DataRow from "../components/DataRow";
+import StatusDataRow from "../components/StatusDataRow";
 
 const AnimalDetailsScreen = ({ route, navigation }) => {
-
   //TODO: animalCtx.getAnimal(id) zamiast route.params
   const {
-      id,
-      name, // TODO
-      type,
-      breed,
-      sex,
-      favourite,
-      adoptable,
-      recently_found,
-      weight,
-      age,
-      city,
-      admission_date,
-      description,
-      imageUrl, // TODO
-      chip_number,
-      is_sterilized,
-      is_vaccinated
+    id,
+    name, // TODO
+    type,
+    breed,
+    sex,
+    favourite,
+    adoptable,
+    recently_found,
+    weight,
+    age,
+    city,
+    admission_date,
+    description,
+    imageUrl, // TODO
+    chip_number,
+    is_sterilized,
+    is_vaccinated,
   } = route.params;
 
   const images = {
     animalType: {
-      'kot': require('../assets/cat_1.jpg'),
-      'pies': require('../assets/dog_1.jpg'),
-      'gad': require('../assets/dog_2.jpg'),
-    }
-  }
+      kot: require("../assets/cat_1.jpg"),
+      pies: require("../assets/dog_1.jpg"),
+      gad: require("../assets/dog_2.jpg"),
+    },
+  };
 
-  return(
+  return (
     <View>
       <ScrollView>
         <View style={styles.container}>
-        <View style={styles.imageView}>
-          <Image 
-            source={images.animalType[type]}
-            style={styles.image}
-          />
-        </View>
-        <View style={styles.groupedRow}>
-          <DataRow label={'Typ'} data={type}/>
-          <View style={styles.heartView}>
-            <HeartIcon favourite={favourite}/>
+          <View style={styles.imageView}>
+            <Image source={images.animalType[type]} style={styles.image} />
           </View>
+          <View style={styles.groupedRow}>
+            <DataRow label={"Typ"} data={type} />
+            <View style={styles.heartView}>
+              <HeartIcon favourite={favourite} />
+            </View>
+          </View>
+          <DataRow label={"W typie rasy"} data={breed} />
+          <DataRow label={"Płeć"} data={sex} />
+          {/*TODO - status*/}
+          <StatusDataRow status={{ adoptable, recently_found }} />
+          <DataRow label={"Waga"} data={weight} />
+          <DataRow label={"Wiek"} data={age} />
+          <DataRow label={"Lokalizacja"} data={city} />
+          <DataRow label={"Data przyjęcia"} data={admission_date} />
+          <DataRow label={"Chip"} data={chip_number}></DataRow>
+          <DataRow label={"Szczepienia"} data={is_vaccinated}></DataRow>
+          <DataRow label={"Sterylizacja"} data={is_sterilized}></DataRow>
+          <DataRow label={"Opis"} data={description}></DataRow>
         </View>
-        <DataRow label={'W typie rasy'} data={breed}/>
-        <DataRow label={'Płeć'} data={sex}/>
-        {/*TODO - status*/}
-        <StatusDataRow status={{adoptable, recently_found}}/>
-        <DataRow label={'Waga'} data={weight}/>
-        <DataRow label={'Wiek'} data={age}/>
-        <DataRow label={'Lokalizacja'} data={city}/>
-        <DataRow label={'Data przyjęcia'} data={admission_date}/>
-        <DataRow label={'Chip'} data={chip_number}></DataRow>
-        <DataRow label={'Szczepienia'} data={is_vaccinated}></DataRow>
-        <DataRow label={'Sterylizacja'} data={is_sterilized}></DataRow>
-        <DataRow label={'Opis'} data={description}></DataRow>
-      </View>
       </ScrollView>
     </View>
-  )
-}
+  );
+};
 export default AnimalDetailsScreen;
 
-const HeartIcon = ({favourite}) => {
-    return(
-        <View>
-          <AntDesign name={favourite ? 'heart' : 'hearto'} size={30} color='black'/> 
-        </View>
-    );
-}
+const HeartIcon = ({ favourite }) => {
+  return (
+    <View>
+      <AntDesign
+        name={favourite ? "heart" : "hearto"}
+        size={30}
+        color="#d12115"
+      />
+    </View>
+  );
+};
 
 //granatowy - #362893
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: '10%',
+    marginHorizontal: "10%",
   },
   imageView: {
-    marginVertical: '5%',
+    marginVertical: "5%",
     paddingBottom: 5,
-    backgroundColor: '#0000',
+    backgroundColor: "#0000",
     borderRadius: 15,
     elevation: 6,
-    shadowColor: '#fff',
-    shadowOffset:{width: 5, height: 5},
+    shadowColor: "#fff",
+    shadowOffset: { width: 5, height: 5 },
     shadowOpacity: 0.5,
     shadowRadius: 3,
   },
   image: {
-    resizeMode: 'cover',
-    width: '100%',
+    resizeMode: "cover",
+    width: "100%",
     height: 200,
     borderRadius: 15,
   },
   heartView: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingRight: '5%'
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingRight: "5%",
   },
   groupedRow: {
     flex: 1,
-    flexDirection: 'row',
-  }
+    flexDirection: "row",
+  },
 });
