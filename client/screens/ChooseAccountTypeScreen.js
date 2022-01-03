092b773fe462fbeb6903086f21a9ab3bd538e23d
Anna Ciszak <aniac600@gmail.com>
2022-01-04 00:01:14 +0100

    Style przycisk typ konta

--- client/screens/ChooseAccountTypeScreen.js
+++ client/screens/ChooseAccountTypeScreen.js
@@ -1,64 +1,84 @@
-import React from 'react';
+import React from "react";
 import {
-  View, Text, StyleSheet, Image, TouchableOpacity, Dimensions, TextInput, ScrollView
-} from 'react-native';
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 const SignInScreen = ({ navigation }) => {
-
-  return(
+  return (
     <View style={styles.container}>
-        <Image
-          source={require('../assets/cat.png')}
-          style={styles.logo}
-        />      
+      <Image source={require("../assets/cat.png")} style={styles.logo} />
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('RegistrationScreen')}>
-        <Text style={{color:'#fff', fontSize: 18, fontWeight: '900', textAlign: 'center' }}>Utwórz konto użytkownika</Text>
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate("RegistrationScreen")}
+        >
+          <Text
+            style={{
+              color: "#fff",
+              fontSize: 18,
+              fontWeight: "900",
+              textAlign: "center",
+            }}
+          >
+            Utwórz konto użytkownika
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity>
-          <Text style={{fontSize: 16, color:'#000', fontWeight: 'bold' }} onPress={() => navigation.navigate('ShelterRegistrationScreen')}>Reprezentujesz schronisko?</Text>
+          <Text
+            style={{ fontSize: 16, color: "#000", fontWeight: "bold" }}
+            onPress={() => navigation.navigate("ShelterRegistrationScreen")}
+          >
+            Reprezentujesz schronisko?
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
 export default SignInScreen;
 
-const {height} = Dimensions.get("screen");
-const height_logo = height * 0.20;
+const { height } = Dimensions.get("screen");
+const height_logo = height * 0.2;
 
 //#362893
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor: '#fff', 
-    alignItems: 'center'
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
 
   logo: {
-    marginTop: height*0.24,
-    marginLeft: '4%',
+    marginTop: height * 0.24,
+    marginLeft: "4%",
     width: height_logo,
     height: height_logo,
   },
-  
+
   buttonContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
 
   loginButton: {
-    flexDirection: 'row',
-    height: 82,
-    width: '45%',
-    marginTop: height*0.05,
-    marginBottom: height*0.13,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#362893',
+    flexDirection: "row",
+    width: 200,
+    marginTop: height * 0.05,
+    marginBottom: height * 0.13,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#362893",
     borderRadius: 20,
     padding: 25,
-  }
+    paddingVertical: 20,
+  },
 });
